042d3b75316b9ede66a32a4ab5527a6c4343e3c6
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-02 17:26:01 +0100

    improve flake

--- flake.nix
+++ flake.nix
@@ -10,60 +10,58 @@
     , flake-utils
     , nixpkgs
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        haskellPackageNames = [
-          "advent-of-code-twentytwo"
-        ];
-        ghcVersion = "ghc924";
-        haskellMkPackage = hps: nm: hps.callCabal2nix nm (./. + "/${nm}") { };
-        haskellOverlay = (
-          selfn: supern: {
-            haskellPackages = supern.haskell.packages.${ghcVersion}.override {
-              overrides = selfh: superh:
-                {
-                  advent-of-code-twentytwo = selfh.callCabal2nix "advent-of-code-twentytwo" ./. rec { };
-                };
-            };
-          }
-        );
-        overlays = [ haskellOverlay ];
-        pkgs = import nixpkgs {
-          inherit system;
-          inherit overlays;
+    let
+      theseHpkgNames = [
+        "advent-of-code-twentytwo"
+      ];
+      thisGhcVersion = "ghc924";
+      hOverlay = selfn: supern: {
+        haskell = supern.haskell // {
+          packageOverrides = selfh: superh:
+            supern.haskell.packageOverrides selfh superh //
+              {
+                advent-of-code-twentytwo = selfh.callCabal2nix "advent-of-code-twentytwo" ./. { };
+              };
         };
-        hpkgs = pkgs.haskellPackages;
-        hlib = pkgs.haskell.lib;
-        advent-of-code-twentytwoPkgs = nixpkgs.lib.genAttrs haskellPackageNames (n: hpkgs.${n});
-        advent-of-code-twentytwoPkgsDev = builtins.mapAttrs (_: x: hlib.doBenchmark x) advent-of-code-twentytwoPkgs;
-      in
-      {
-        packages = advent-of-code-twentytwoPkgs // { default = advent-of-code-twentytwoPkgs.advent-of-code-twentytwo; };
+      };
+      perSystem = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ hOverlay ];
+          };
+          hpkgs = pkgs.haskell.packages.${thisGhcVersion};
+          hlib = pkgs.haskell.lib;
+          theseHpkgs = nixpkgs.lib.genAttrs theseHpkgNames (n: hpkgs.${n});
+          theseHpkgsDev = builtins.mapAttrs (_: x: hlib.doBenchmark x) theseHpkgs;
+        in
+        {
+          packages = theseHpkgs // { default = theseHpkgs.advent-of-code-twentytwo; };
 
-        devShells.default = hpkgs.shellFor {
-          # shellHook =
-          #   let
-          #     scripts = ./scripts;
-          #   in
-          #   ''
-          #     export PATH="${scripts}:$PATH"
-          #   '';
-          packages = _: (builtins.attrValues advent-of-code-twentytwoPkgsDev);
-          nativeBuildInputs = with pkgs; [
-            # See https://github.com/NixOS/nixpkgs/issues/59209.
-            bashInteractive
+          devShells.default = hpkgs.shellFor {
+            # shellHook =
+            #   let
+            #     scripts = ./scripts;
+            #   in
+            #   ''
+            #     export PATH="${scripts}:$PATH"
+            #   '';
+            packages = _: (builtins.attrValues theseHpkgsDev);
+            nativeBuildInputs = with pkgs; [
+              # See https://github.com/NixOS/nixpkgs/issues/59209.
+              bashInteractive
 
-            # Haskell toolchain.
-            hpkgs.cabal-fmt
-            hpkgs.cabal-install
-            hpkgs.haskell-language-server
-          ];
-          buildInputs = with pkgs; [
-          ];
-          doBenchmark = true;
-          # withHoogle = true;
+              # Haskell toolchain.
+              hpkgs.cabal-fmt
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+            ];
+            buildInputs = with pkgs; [
+            ];
+            doBenchmark = true;
+            # withHoogle = true;
+          };
         };
-      }
-    );
+    in
+    { overlays.default = hOverlay; } // flake-utils.lib.eachDefaultSystem perSystem;
 }
