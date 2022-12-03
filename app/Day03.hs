5a27827dd3cba0a2446d292130c485024545a156
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-03 07:57:18 +0100

    day03 start

---
+++ app/Day03.hs
@@ -0,0 +1,47 @@
+-- |
+-- Module      :  Day03
+-- Description :  Rucksacks
+-- Copyright   :  2022 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Sat Dec  3 07:36:42 2022.
+module Main
+  ( main,
+  )
+where
+
+import Control.Applicative
+import Data.Attoparsec.Text
+import Data.Maybe
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+assembleSet :: T.Text -> S.Set Char
+assembleSet = T.foldl' (flip S.insert) S.empty
+
+findDuplicate :: T.Text -> Char
+findDuplicate x = fromJust $ T.find (`S.member` s) x2
+  where
+    l = T.length x
+    l2 = l `div` 2
+    (x1, x2) = T.splitAt l2 x
+    s = assembleSet x1
+
+pRucksack :: Parser T.Text
+pRucksack = takeWhile1 (not . isEndOfLine)
+
+pInput :: Parser [T.Text]
+pInput = pRucksack `sepBy1` endOfLine <* optional endOfLine <* endOfInput
+
+main :: IO ()
+main = do
+  t <- T.readFile "inputs/input03.txt"
+  let xs = either error id $ parseOnly pInput t
+      ds = map findDuplicate xs
+  print xs
+  print ds
