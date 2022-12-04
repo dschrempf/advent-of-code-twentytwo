40c18c308e71eb81a7dbaef8f327ccaef93aa5a1
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-04 09:56:58 +0100

    day 4 stub

---
+++ app/Day04.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      :  Day04
+-- Description :  Assignments
+-- Copyright   :  2022 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Sun Dec  4 09:55:52 2022.
+module Day04
+  ( main,
+  )
+where
+
+main :: IO ()
+main = undefined
