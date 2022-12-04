e64dda1021f3f94c81221ed3497ed3142bdec09a
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-04 10:40:47 +0100

    day 4, solution

--- app/Day04.hs
+++ app/Day04.hs
@@ -9,15 +9,18 @@
 -- Portability :  portable
 --
 -- Creation date: Sun Dec  4 09:55:52 2022.
-module Day04
+module Main
   ( main,
   )
 where
 
+import Control.Applicative
 import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as BS
 import Numeric.Natural
 
 data Assignment = Assignment Natural Natural
+  deriving (Show)
 
 pAssignment :: Parser Assignment
 pAssignment = do
@@ -27,7 +30,35 @@ pAssignment = do
   pure $ Assignment x y
 
 pPair :: Parser (Assignment, Assignment)
-pPair = undefined
+pPair = do
+  x <- pAssignment
+  _ <- char ','
+  y <- pAssignment
+  pure (x, y)
+
+pInput :: Parser [(Assignment, Assignment)]
+pInput = pPair `sepBy1'` endOfLine <* optional endOfLine <* endOfInput
+
+contains :: Assignment -> Assignment -> Bool
+contains (Assignment a b) (Assignment c d) = a <= c && b >= d
+
+contain :: Assignment -> Assignment -> Bool
+contain x y = contains x y || contains y x
+
+overlaps :: Assignment -> Assignment -> Bool
+overlaps (Assignment a b) (Assignment c d)
+  | a <= c = b >= c
+  | b >= d = a <= d
+  | otherwise = False
+
+overlap :: Assignment -> Assignment -> Bool
+overlap x y = overlaps x y || overlaps y x
 
 main :: IO ()
-main = undefined
+main = do
+  b <- BS.readFile "inputs/input04.txt"
+  let ps = either error id $ parseOnly pInput b
+  -- Part 1.
+  print $ length $ filter (== True) $ map (uncurry contain) ps
+  -- Part 2.
+  print $ length $ filter (== True) $ map (uncurry overlap) ps
