ad996b392d3d6444c283a929bd1042b721ee4af7
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-05 23:58:29 +0100

    day 6 stub

---
+++ app/Day06.hs
@@ -0,0 +1,20 @@
+-- |
+-- Module      :  Main
+-- Description :  Day 6; ?
+-- Copyright   :  2022 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Mon Dec  5 23:57:50 2022.
+--
+-- See https://adventofcode.com/2022/day/6.
+module Main
+  ( main,
+  )
+where
+
+main :: IO ()
+main = undefined
