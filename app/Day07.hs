cd2c74764eb4af155db1deb65e3e2e8c190495c0
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-08 20:04:49 +0100

    parser

--- app/Day07.hs
+++ app/Day07.hs
@@ -29,24 +29,35 @@ data File = File
   { fName :: TS.Text,
     fSize :: Integer
   }
+  deriving (Show)
 
-data DirectoryTree = Directory
+data DirTree = DirNode
   { dName :: TS.Text,
     files :: [File],
-    subdirectories :: [DirectoryTree]
+    subdirectories :: [DirTree]
   }
+  deriving (Show)
 
-data Command = CdUp | CdDir TS.Text | Ls
+data CdCmd = CdRoot | CdUp | CdDir TS.Text
+  deriving (Show, Eq)
+
+data Cmd = Cd CdCmd | Ls
   deriving (Show, Eq)
 
 pAtom :: Parser TS.Text
 pAtom = takeWhile1 (not . isSpace)
 
-pCommand :: Parser Command
-pCommand = string "$ " *> (pCdUp <|> pCdDir <|> pLs) <* optional endOfLine
+pCdCmd :: Parser CdCmd
+pCdCmd = string "cd " *> (pCdRoot <|> pCdUp <|> pCdDir)
   where
-    pCdUp = CdUp <$ string "cd .."
-    pCdDir = CdDir <$> (string "cd " *> pAtom)
+    pCdRoot = CdRoot <$ char '/'
+    pCdUp = CdUp <$ string ".."
+    pCdDir = CdDir <$> pAtom
+
+pCmd :: Parser Cmd
+pCmd = string "$ " *> (pCd <|> pLs)
+  where
+    pCd = Cd <$> pCdCmd
     pLs = Ls <$ string "ls"
 
 pFile :: Parser File
@@ -54,19 +65,26 @@ pFile = do
   d <- decimal
   _ <- char ' '
   n <- pAtom
-  _ <- optional endOfLine
   pure $ File n d
 
-pDirectory :: Parser DirectoryTree
-pDirectory = string "dir " *> pDir <* optional endOfLine
-  where
-    toDir n = Directory n [] []
-    pDir = toDir <$> pAtom
+newtype Dir = Dir {getDirName :: TS.Text}
 
-pInput :: Parser DirectoryTree
-pInput = undefined
+pDir :: Parser Dir
+pDir = string "dir " *> (Dir <$> pAtom)
+
+data Line = LCmd Cmd | LFile File | LDir Dir
+
+pLine :: Parser Line
+pLine = (LCmd <$> pCmd) <|> (LFile <$> pFile) <|> (LDir <$> pDir)
+
+pInput :: Parser [Line]
+pInput = pLine `sepBy1'` endOfLine <* optional endOfLine <* endOfInput
+
+-- case c of
+--   (Cd CdRoot) -> pDirTree (DirNode "/" [] [])
+--   _ -> error "pInput: no cd /"
 
 main :: IO ()
 main = do
   b <- TL.readFile "inputs/input07-sample.txt"
-  print ""
+  print $ parseOnly pInput b
