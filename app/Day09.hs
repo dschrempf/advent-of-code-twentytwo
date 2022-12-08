eaaed1800ab2a1b050998c4bcda5002adf2e2d1c
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-08 23:29:40 +0100

    day 9 stub

---
+++ app/Day09.hs
@@ -0,0 +1,20 @@
+-- |
+-- Module      :  Main
+-- Description :  Day9; ?
+-- Copyright   :  2022 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Thu Dec  8 23:29:13 2022.
+--
+-- See https://adventofcode.com/2022/day/0.
+module Main
+  ( main,
+  )
+where
+
+main :: IO ()
+main = undefined
