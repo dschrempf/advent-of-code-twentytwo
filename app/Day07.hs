382edefb8ef6fedeb239f68f5d6d3e2ade22b5dd
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-08 20:38:54 +0100

    first handlers

--- app/Day07.hs
+++ app/Day07.hs
@@ -21,18 +21,18 @@ where
 import Control.Applicative
 import Data.Attoparsec.Text.Lazy
 import Data.Char
+import Data.List
 import qualified Data.Text as TS
-import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
 
 data File = File
   { fName :: TS.Text,
     fSize :: Integer
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data DirTree = DirNode
-  { dName :: TS.Text,
+  { label :: TS.Text,
     files :: [File],
     subdirectories :: [DirTree]
   }
@@ -68,11 +68,13 @@ pFile = do
   pure $ File n d
 
 newtype Dir = Dir {getDirName :: TS.Text}
+  deriving (Show)
 
 pDir :: Parser Dir
 pDir = string "dir " *> (Dir <$> pAtom)
 
 data Line = LCmd Cmd | LFile File | LDir Dir
+  deriving (Show)
 
 pLine :: Parser Line
 pLine = (LCmd <$> pCmd) <|> (LFile <$> pFile) <|> (LDir <$> pDir)
@@ -80,11 +82,41 @@ pLine = (LCmd <$> pCmd) <|> (LFile <$> pFile) <|> (LDir <$> pDir)
 pInput :: Parser [Line]
 pInput = pLine `sepBy1'` endOfLine <* optional endOfLine <* endOfInput
 
--- case c of
---   (Cd CdRoot) -> pDirTree (DirNode "/" [] [])
---   _ -> error "pInput: no cd /"
+-- Add a directory to an existing list of directories.
+--
+-- Meh, this function turned out to be a bit complicated. The problem is, we do
+-- not know if the directory is already in the list. Also, we do not know if all
+-- the sub-(sub-...) are in the list, so we have to recursively go through them
+-- (see 'mergeDirs').
+addDir :: [DirTree] -> DirTree -> [DirTree]
+addDir [] d = [d]
+addDir (x : xs) d =
+  if label d == label x
+    then mergeDirs d x : xs
+    else x : addDir xs d
+  where
+    mergeDirs (DirNode n fs1 ds1) (DirNode _ fs2 ds2) =
+      DirNode n (fs1 `union` fs2) $ foldl' addDir ds1 ds2
+
+hDirTree :: DirTree -> [Line] -> (DirTree, [Line])
+hDirTree d@(DirNode n fs ds) (x : xs) = case x of
+  (LCmd c) -> case c of
+    -- Ignore 'ls' commands.
+    Ls -> hDirTree d xs
+    -- Only handle one level changes.
+    (Cd CdRoot) -> error "hDirTree: jump to root"
+    (Cd CdUp) -> (d, xs)
+    (Cd (CdDir n')) ->
+      let (d', xs') = hDirTree (DirNode n' [] []) xs
+          ds' = addDir ds d'
+       in (DirNode n fs ds', xs')
+
+hInput :: [Line] -> DirTree
+hInput ((LCmd (Cd CdRoot)) : xs) = fst $ hDirTree (DirNode "/" [] []) xs
+hInput _ = error "hInput: no cd /"
 
 main :: IO ()
 main = do
   b <- TL.readFile "inputs/input07-sample.txt"
-  print $ parseOnly pInput b
+  let ls = either error id $ parseOnly pInput b
+  print $ hInput ls
