78b232501b9069a1a7d1f1100f3bd11d178505a3
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-10 08:42:47 +0100

    day10, part 1

--- app/Day10.hs
+++ app/Day10.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 10; Cathode-Ray Tube
@@ -16,5 +19,46 @@ module Main
   )
 where
 
+import Control.Applicative
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as BS
+import Data.List
+
+newtype X = X Int
+  deriving (Show, Eq)
+  deriving (Num) via Int
+
+data Instruction = Noop | Addx Int
+  deriving (Show, Eq)
+
+pInstruction :: Parser Instruction
+pInstruction = noop <|> addx
+  where
+    noop = Noop <$ string "noop"
+    addx = Addx <$> (string "addx " *> signed decimal)
+
+pInput :: Parser [Instruction]
+pInput = pInstruction `sepBy1'` endOfLine <* optional endOfLine <* endOfInput
+
+-- Part 1.
+
+evalStep :: [X] -> Instruction -> [X]
+evalStep [] _ = error "evalStep: empty register"
+evalStep (x : xs) Noop = x : x : xs
+evalStep (x : xs) (Addx n) = (x + X n) : x : x : xs
+
+signalStrengths :: [X] -> [X]
+signalStrengths xs = [X i * (xs !! pred i) | i <- [20, 60 .. l]]
+  where
+    l = length xs
+
 main :: IO ()
-main = undefined
+main = do
+  b <- BS.readFile "inputs/input10.txt"
+  -- Part 1.
+  let is = either error id $ parseOnly pInput b
+      cs = reverse $ foldl' evalStep [X 1] is
+      ss = signalStrengths cs
+  print $ length ss
+  print ss
+  print $ sum ss
