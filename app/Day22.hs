835353faca9f727c50625577ed4b9ff71c5fab02
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-12 10:29:54 +0100

    stubs

---
+++ app/Day22.hs
@@ -0,0 +1,20 @@
+-- |
+-- Module      :  Main
+-- Description :  Day 22; ?
+-- Copyright   :  2022 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Fri Dec  9 09:10:38 2022.
+--
+-- See https://adventofcode.com/2022/day/22.
+module Main
+  ( main,
+  )
+where
+
+main :: IO ()
+main = undefined
