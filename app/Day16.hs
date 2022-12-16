7110f540d18e0ca7205d9b90c8f03ba202449d1d
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-16 23:14:30 +0100

    part 1 much faster; misses paths for full input

--- app/Day16.hs
+++ app/Day16.hs
@@ -20,14 +20,15 @@ where
 
 import Aoc.Function
 import Control.Applicative
-import Control.Monad
+import Control.Monad hiding (sequence)
 import Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString.Char8 as BS
 import Data.Foldable
+import qualified Data.Map.Strict as M
 import Data.Maybe
-import qualified Data.PartialOrd as P
 import qualified Data.Set as S
 import Debug.Pretty.Simple
+import Prelude hiding (sequence)
 
 data Valve = Valve
   { name :: String,
@@ -60,63 +61,89 @@ type Valves = S.Set Valve
 pInput :: Parser [Valve]
 pInput = pValve `sepBy1` endOfLine <* optional endOfLine <* endOfInput <?> "input"
 
+-- Part 1.
+
 data State = State
-  { minutesLeft :: Int,
-    opened :: Valves,
+  { opened :: Valves,
+    sequence :: [(Int, Valve)],
     current :: Valve,
     released :: Int
   }
   deriving (Show, Eq)
 
-instance P.PartialOrd State where
-  (State mx ox cx rx) <= (State my oy cy ry)
-    -- We only want to compare states at the same time here.
-    | mx /= my = error "should not happen"
-    | cx == cy = totx < toty
-    | otherwise = False
-    where
-      totx = totalRate ox * mx + rx
-      toty = totalRate oy * mx + ry
+-- Actually a partial order, but we avoid testing non-compareable elements. If
+-- we do so by accident, however, we fail.
+compareStates :: Int -> State -> State -> Ordering
+compareStates m x y
+  | current x /= current y = error "bug: current valves differ"
+  | otherwise = compare (released x + yb) (released y + xb)
+  where
+    xb = backwards m (opened y) $ sequence x
+    yb = backwards m (opened x) $ sequence y
 
--- Part 1.
-totalRate :: Valves -> Int
-totalRate = sum . S.map flowRate
+-- Calculate released pressure walking the path backwards.
+backwards :: Int -> Valves -> [(Int, Valve)] -> Int
+backwards m os = snd . foldl' f (0, 0)
+  where
+    f (nskipped, tot) (n, x)
+      | m' >= 30 = (nskipped, tot)
+      | x `S.member` os = (succ nskipped, tot)
+      | otherwise = (nskipped, releaseAt m' x + tot)
+      where
+        m' = m + (m - n) + 1 - nskipped
 
-release :: State -> State
-release (State m xs c r) = State m xs c $ r + totalRate xs
+data Trace = Trace
+  { _minute :: Int,
+    -- Map from current valve to set of attained states.
+    _traces :: M.Map Valve State
+  }
+  deriving (Show, Eq)
 
-open :: State -> State
-open (State m xs c r) = State (pred m) (S.insert c xs) c r
+-- Total released pressure over the remaining time.
+releaseAt :: Int -> Valve -> Int
+releaseAt m x = flowRate x * (30 - m)
+
+-- Assume that valve has not been opened yet.
+open :: Int -> State -> State
+open m (State xs ss c r) = State (S.insert c xs) ((m, c) : ss) c (r + releaseAt m c)
 
 move :: Valves -> State -> [State]
-move vs (State m xs c r) =
-  [ State (pred m) xs c' r
+move vs (State xs ss c r) =
+  [ State xs ss c' r
     | t <- tunnels c,
       let c' = fromJust $ find ((== t) . name) vs
   ]
 
-lap :: Valves -> State -> [State]
-lap vs x = open x' : move vs x'
+openOrMove :: Int -> Valves -> State -> [State]
+openOrMove m vs x@(State xs _ c _)
+  -- Do not open valve if already open.
+  | c `S.member` xs = move vs x
+  | otherwise = open m x : move vs x
+
+sortIntoMap :: Int -> [State] -> M.Map Valve State
+sortIntoMap m = foldl' insertBetter M.empty
   where
-    x' = release x
+    insertBetter mp x = M.alter (findBetter x) (current x) mp
+    findBetter x Nothing = Just x
+    findBetter x (Just y) = Just $ case compareStates m x y of
+      LT -> y
+      _ -> x
 
-clean :: [State] -> [State]
-clean = P.maxima
-
-next :: Valves -> [State] -> [State]
-next vs xs = case compare (minutesLeft (head xs)) 1 of
-  LT -> error "next: out of minutes"
-  EQ -> map release xs
-  GT -> clean $ concatMap (lap vs) xs
+next :: Valves -> Trace -> Trace
+next vs (Trace m xs) = case compare m 30 of
+  GT -> error "next: out of minutes"
+  _ -> Trace (succ m) $ sortIntoMap m $ concatMap (openOrMove m vs) $ M.elems xs
 
 main :: IO ()
 main = do
   d <- BS.readFile "inputs/input16-sample.txt"
   let xs = either error id $ parseOnly pInput d
       vs = S.fromList xs
-      s0 = State 30 S.empty (head xs) 0
+      x0 = head xs
+      s0 = State S.empty [] x0 0
+      t0 = Trace 1 $ M.singleton x0 s0
       -- First move.
-      ss = move vs s0
-      r1 = nTimes 28 (next vs) ss
-      r0 = map release r1
-  pTraceShowM $ map released r0
+      (Trace _ m) = nTimes 30 (next vs) t0
+  pTraceShowM $ maximum $ map released $ M.elems m
+  let f x y = compare (released x) (released y)
+  pTraceShowM $ maximumBy f $ M.elems m
