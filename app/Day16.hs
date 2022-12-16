9eec5fa4f7d0a60a126e8e2f4f66ecb13a99dcc2
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-16 10:05:55 +0100

    day 16 parser

--- app/Day16.hs
+++ app/Day16.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 16; ?
@@ -16,5 +18,42 @@ module Main
   )
 where
 
+import Control.Applicative
+import Control.Monad
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as BS
+
+data Valve = Valve
+  { name :: String,
+    flowRate :: Int,
+    tunnels :: [String]
+  }
+  deriving (Show)
+
+pValveName :: Parser String
+pValveName = count 2 letter_ascii <?> "vname"
+
+pTunnelString :: Parser ()
+pTunnelString =
+  void $
+    string "; tunnels lead to valves "
+      <|> string "; tunnel leads to valve "
+
+pValve :: Parser Valve
+pValve = do
+  _ <- string "Valve "
+  n <- pValveName
+  _ <- string " has flow rate="
+  r <- decimal
+  _ <- pTunnelString
+  ts <- pValveName `sepBy1'` string ", "
+  pure $ Valve n r ts
+
+pInput :: Parser [Valve]
+pInput = pValve `sepBy1` endOfLine <* optional endOfLine <* endOfInput <?> "input"
+
 main :: IO ()
-main = undefined
+main = do
+  d <- BS.readFile "inputs/input16-sample.txt"
+  let vs = either error id $ parseOnly pInput d
+  print vs
