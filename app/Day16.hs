a812900e33ad99ab41dd65cd1388268962b6a65a
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-17 22:54:49 +0100

    day 16 part 2, first take

--- app/Day16.hs
+++ app/Day16.hs
@@ -81,31 +81,33 @@ instance NFData Path
 
 -- Actually a partial order, but we avoid testing non-compareable elements. If
 -- we do so by accident, we fail.
-comparePaths :: Int -> Path -> Path -> Ordering
-comparePaths m x y
+comparePaths :: Int -> Int -> Path -> Path -> Ordering
+comparePaths mnow mmax x y
   | current x /= current y = error "comparePaths: bug: current valves differ"
   | otherwise = compare (released x + yb) (released y + xb)
   where
-    xb = backwards m (opened y) $ sequence x
-    yb = backwards m (opened x) $ sequence y
+    xb = backwards mnow mmax (opened y) $ sequence x
+    yb = backwards mnow mmax (opened x) $ sequence y
 
 -- Calculate released pressure walking the path backwards.
 backwards ::
   -- Current minute.
+  Int ->
+  -- Maximum minutes.
   Int ->
   -- Opened valves.
   Valves ->
   -- Sequence of other path.
   [(Int, Valve)] ->
   Int
-backwards m os = snd . foldl' f (0, 0)
+backwards mnow mmax os = snd . foldl' f (0, 0)
   where
     f (nskipped, tot) (n, x)
-      | m' >= 30 = (nskipped, tot)
+      | mnow' >= mmax = (nskipped, tot)
       | x `S.member` os = (succ nskipped, tot)
-      | otherwise = (nskipped, releaseAt m' x + tot)
+      | otherwise = (nskipped, releaseAt mnow' mmax x + tot)
       where
-        m' = m + (m - n) + 1 - nskipped
+        mnow' = mnow + (mnow - n) + 1 - nskipped
 
 -- Map from current valve to list of equivalent paths.
 type Map = M.Map Valve [Path]
@@ -119,14 +121,14 @@ data Trace = Trace
 instance NFData Trace
 
 -- Total released pressure over the remaining time.
-releaseAt :: Int -> Valve -> Int
-releaseAt m x = flowRate x * (30 - m)
+releaseAt :: Int -> Int -> Valve -> Int
+releaseAt mnow mmax x = flowRate x * (mmax - mnow)
 
 -- Assume the valve has not been opened yet. Also reset previously visited paths
 -- to enable movement in backward direction.
-open :: Int -> Path -> Path
-open m (Path xs ss c _ r) =
-  Path (S.insert c xs) ((m, c) : ss) c (S.singleton c) (r + releaseAt m c)
+open :: Int -> Int -> Path -> Path
+open mnow mmax (Path xs ss c _ r) =
+  Path (S.insert c xs) ((mnow, c) : ss) c (S.singleton c) (r + releaseAt mnow mmax c)
 
 move :: Valves -> Path -> [Path]
 move vs (Path xs ss c p r) =
@@ -138,16 +140,16 @@ move vs (Path xs ss c p r) =
       let p' = S.insert c' p
   ]
 
-openOrMove :: Int -> Valves -> Path -> [Path]
-openOrMove m vs x@(Path xs _ c _ _)
+openOrMove :: Int -> Int -> Valves -> Path -> [Path]
+openOrMove mnow mmax vs x@(Path xs _ c _ _)
   -- Do not open valves without flow rate.
   | flowRate c == 0 = move vs x
   -- Do not open valves already open.
   | c `S.member` xs = move vs x
-  | otherwise = open m x : move vs x
+  | otherwise = open mnow mmax x : move vs x
 
-sortIntoMap :: Int -> [Path] -> Map
-sortIntoMap m = foldl' insertBetter M.empty
+sortIntoMap :: Int -> Int -> [Path] -> Map
+sortIntoMap mnow mmax = foldl' insertBetter M.empty
   where
     insertBetter mp x = M.alter (Just . findBetter x) (current x) mp
     findBetter x Nothing = [x]
@@ -158,22 +160,102 @@ sortIntoMap m = foldl' insertBetter M.empty
       | GT `elem` cs = x : catMaybes (zipWith p cs ys)
       | otherwise = ys
       where
-        cs = map (comparePaths m x) ys
+        cs = map (comparePaths mnow mmax x) ys
     p GT _ = Nothing
     p _ y = Just y
 
-next :: Valves -> Trace -> Trace
-next vs (Trace m xs) = case compare m 30 of
-  GT -> error "next: out of minutes"
-  _ -> Trace (succ m) $ sortIntoMap m $ concatMap (openOrMove m vs) $ concat $ M.elems xs
+next :: Int -> Valves -> Trace -> Trace
+next mmax vs (Trace mnow xs) =
+  case compare mnow mmax of
+    GT -> error "next: out of minutes"
+    _ -> Trace (succ mnow)
+    $ sortIntoMap mnow mmax
+    $ concatMap (openOrMove mnow mmax vs)
+    $ concat
+    $ M.elems xs
+
+-- Part 2.
+
+data Path2 = Path2 Path Path
+  deriving (Show, Eq, Generic)
+
+instance NFData Path2
+
+comparePaths2 :: Int -> Int -> Path2 -> Path2 -> Ordering
+comparePaths2 mnow mmax (Path2 x1 x2) (Path2 y1 y2)
+  | current x1 /= current y1 = error "comparePath2: bug: current valves of path 1 differ"
+  | current x2 /= current y2 = error "comparePath2: bug: current valves of path 2 differ"
+  | otherwise = case (comparePaths mnow mmax x1 y1, comparePaths mnow mmax x2 y2) of
+      (GT, GT) -> GT
+      (LT, LT) -> LT
+      (_, _) -> EQ
+
+type Map2 = M.Map (Valve, Valve) [Path2]
+
+data Trace2 = Trace2
+  { _minute2 :: Int,
+    _paths2 :: Map2
+  }
+  deriving (Show, Eq, Generic)
+
+instance NFData Trace2
+
+openOrMove2 :: Int -> Int -> Valves -> Path2 -> [Path2]
+openOrMove2 mnow mmax vs p@(Path2 x y)
+  | null ps2' = [p]
+  | otherwise = ps2'
+  where
+    xs = openOrMove mnow mmax vs x
+    ys = openOrMove mnow mmax vs y
+    ps2' =
+      [ Path2 a' b'
+        | a <- xs,
+          b <- ys,
+          opened a `S.disjoint` opened b,
+          let os = opened a `S.union` opened b,
+          let a' = a {opened = os},
+          let b' = b {opened = os}
+      ]
+
+sortIntoMap2 :: Int -> Int -> [Path2] -> Map2
+sortIntoMap2 mnow mmax = foldl' insertBetter M.empty
+  where
+    insertBetter mp x@(Path2 a b) = M.alter (Just . findBetter x) (current a, current b) mp
+    findBetter x Nothing = [x]
+    findBetter x (Just ys)
+      -- If the new path is better than all others, use it exclusively.
+      | all (== GT) cs = [x]
+      -- If the new path is better than some others, use the best paths.
+      | GT `elem` cs = x : catMaybes (zipWith p cs ys)
+      | otherwise = ys
+      where
+        cs = map (comparePaths2 mnow mmax x) ys
+    p GT _ = Nothing
+    p _ y = Just y
+
+next2 :: Int -> Valves -> Trace2 -> Trace2
+next2 mmax vs (Trace2 mnow xs) =
+  case compare mnow mmax of
+    GT -> error "next: out of minutes"
+    _ -> Trace2 (succ mnow)
+    $ sortIntoMap2 mnow mmax
+    $ concatMap (openOrMove2 mnow mmax vs)
+    $ concat
+    $ M.elems xs
 
 main :: IO ()
 main = do
   d <- BS.readFile "inputs/input16-sample.txt"
+  -- Part 1.
   let xs = either error id $ parseOnly pInput d
       vs = S.fromList xs
       x0 = fromJust $ find ((== "AA") . name) xs
-      s0 = Path S.empty [] x0 (S.singleton x0) 0
-      t0 = Trace 1 $ M.singleton x0 [s0]
-      (Trace _ m) = nTimes 30 (next vs) t0
+      p0 = Path S.empty [] x0 (S.singleton x0) 0
+      t0 = Trace 1 $ M.singleton x0 [p0]
+      (Trace _ m) = nTimes 30 (next 30 vs) t0
   pTraceShowM $ maximum $ map released $ concat $ M.elems m
+  -- Part 2.
+  let p20 = Path2 p0 p0
+      t20 = Trace2 1 $ M.singleton (x0, x0) [p20]
+      (Trace2 _ m2) = nTimes 26 (next2 26 vs) t20
+  pTraceShowM $ maximum $ map (\(Path2 a b) -> released a + released b) $ concat $ M.elems m2
