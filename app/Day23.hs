4e0598e992873f32b43396d293c25a95a83ae453
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-12-23 10:53:58 +0100

    day23, parsing and printing

--- app/Day23.hs
+++ app/Day23.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      :  Main
--- Description :  Day 23; ?
+-- Description :  Day 23; Unstable Diffusion
 -- Copyright   :  2022 Dominik Schrempf
 -- License     :  GPL-3.0-or-later
 --
@@ -16,5 +16,47 @@ module Main
   )
 where
 
+import Control.Applicative
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as BS
+import Data.Massiv.Array
+  ( Array,
+    B,
+    Comp (..),
+    Ix2,
+  )
+import qualified Data.Massiv.Array as A
+
+data Tile = Elf | Ground
+  deriving (Show, Eq)
+
+toChar :: Tile -> Char
+toChar Elf = '#'
+toChar Ground = '.'
+
+pTile :: Parser Tile
+pTile = Ground <$ char '.' <|> Elf <$ char '#'
+
+isElf :: Tile -> Bool
+isElf Elf = True
+isElf Ground = False
+{-# INLINE isElf #-}
+
+isGround :: Tile -> Bool
+isGround Elf = False
+isGround Ground = True
+{-# INLINE isGround #-}
+
+type Field = Array B Ix2 Tile
+
+pField :: Parser Field
+pField = A.fromLists' Seq <$> some pTile `sepBy1'` endOfLine <* optional endOfLine <* endOfInput
+
+showField :: Field -> String
+showField = unlines . map (map toChar) . A.toLists
+
 main :: IO ()
-main = undefined
+main = do
+  d <- BS.readFile "inputs/input23-sample.txt"
+  let f = either error id $ parseOnly pField d
+  putStrLn $ showField f
